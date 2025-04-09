3ea06e04a5c96196eed0c48eb9a628cd0dcf7404
nour <nbenmoulehom@gmail.com>
2025-04-09 13:44:41 +0100

    °inital commit

---
+++ simple-life/src/main/java/com/simply/simple_life/SimpleLifeApplication.java
@@ -0,0 +1,13 @@
+package com.simply.simple_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLifeApplication.class, args);
+	}
+
+}
---
+++ simple-life/src/test/java/com/simply/simple_life/SimpleLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.simple_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
